bbf97c33b0198f4d373fbcd8f14059456aefdcee
Abraham Ruiz <jesus.ruiz279158@potros.itson.edu.mx>
2026-04-23 07:58:26 -0700

    Nuevos Paquetes

--- OneDrive/Documentos/NetBeansProjects/F1EngineManager/src/Modelos/Empleadosf1.java
+++ OneDrive/Documentos/NetBeansProjects/F1EngineManager/src/Modelos/Empleadosf1.java
@@ -1,14 +1,14 @@
-package f1enginemanager.F1EngineManager;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package Modelos;
 
 /**
  *
  * @author abrah
  */
-public class GestorEscuderia {
+public class Empleadosf1 {
     
+    private String nombre;
 }
--- OneDrive/Documentos/NetBeansProjects/F1EngineManager/src/Modelos/GestorEscuderia.java
+++ OneDrive/Documentos/NetBeansProjects/F1EngineManager/src/Modelos/GestorEscuderia.java
@@ -1,14 +1,14 @@
+package Modelos;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package f1enginemanager.F1EngineManager;
 
 /**
  *
  * @author abrah
  */
-public class Empleadosf1 {
+public class GestorEscuderia {
     
-    private String nombre;
 }
