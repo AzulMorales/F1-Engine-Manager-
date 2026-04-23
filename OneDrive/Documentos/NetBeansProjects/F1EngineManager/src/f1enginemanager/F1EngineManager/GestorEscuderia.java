77e196e2c60e568f7781e838f17d7f77f1c54fa1
Abraham Ruiz <jesus.ruiz279158@potros.itson.edu.mx>
2026-04-23 07:12:34 -0700

    Clase Gestor Empleados

--- OneDrive/Documentos/NetBeansProjects/F1EngineManager/src/f1enginemanager/F1EngineManager/GestorEscuderia.java
+++ OneDrive/Documentos/NetBeansProjects/F1EngineManager/src/f1enginemanager/F1EngineManager/GestorEscuderia.java
@@ -1,3 +1,5 @@
+package f1enginemanager.F1EngineManager;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
